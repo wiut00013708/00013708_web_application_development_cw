53cc9cf27113505153c02fb423f6d9be92a3df42
wiut00013708 <wiut00013708@gmail.com>
2024-03-20 17:34:27 +0500

    Create connection string & dependency injection

---
+++ API/00013708_web_application_api/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using _00013708_web_application_api.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace _00013708_web_application_api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<PhoneNumber> PhoneNumbers { get; set; }
+
+    }
+}
