a9285a5b9b9e44d597c14609f0c30db90c401f44
wiut00013708 <wiut00013708@gmail.com>
2024-03-20 19:06:21 +0500

    add controller to handle crud operations for contacts class

---
+++ API/00013708_web_application_api/Controllers/ContactsController.cs
@@ -0,0 +1,39 @@
+﻿using _00013708_web_application_api.Data;
+using _00013708_web_application_api.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _00013708_web_application_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public ContactsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public List<Contact> GetAll()
+        {
+            return dbContext.Contacts.ToList();
+        }
+
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> CreateContact(Contact contact)
+        {
+            await dbContext.Contacts.AddAsync(contact);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(contact);
+
+        }
+
+    }
+}
