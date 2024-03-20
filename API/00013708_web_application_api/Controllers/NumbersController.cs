c351341a52230de473e6b9ef0aa1acea33559ecf
wiut00013708 <wiut00013708@gmail.com>
2024-03-20 20:38:55 +0500

    Create CRUD operations for contacts and Numbers

--- API/00013708_web_application_api/Controllers/NumbersController.cs
+++ API/00013708_web_application_api/Controllers/NumbersController.cs
@@ -1,5 +1,4 @@
-﻿using _00013708_web_application_api.Data;
-using _00013708_web_application_api.Models.Domain;
+﻿using _00013708_web_application_api.Models.Domain;
 using _00013708_web_application_api.Repositories.Interface;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -8,45 +7,45 @@ namespace _00013708_web_application_api.Controllers
 {
     [Route("api/[controller]/[action]")]
     [ApiController]
-    public class ContactsController : ControllerBase
+    public class NumbersController : ControllerBase
     {
-        private readonly IContactRepository<Contact> _repository;
-
-        public ContactsController(IContactRepository<Contact> repository)
+        private readonly IContactRepository<PhoneNumber> _repository;
+        public NumbersController(IContactRepository<PhoneNumber> repository)
         {
             _repository = repository;
         }
 
         [HttpGet]
-        public async Task<IEnumerable<Contact>> GetAll() => await _repository.GetAllAsync();
-
-
+        public async Task<IEnumerable<PhoneNumber>> GetAll() => await _repository.GetAllAsync();
 
         [HttpGet("{id}")]
-        [ProducesResponseType(typeof(Contact), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(PhoneNumber), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetByID(int id)
         {
-            var cont = await _repository.GetByIDAsync(id);
-            return cont == null ? NotFound() : Ok(cont);
+            var numberItem = await _repository.GetByIDAsync(id);
+            return numberItem == null ? NotFound() : Ok(numberItem);
         }
 
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
-        public async Task<IActionResult> Create(Contact cont)
+        public async Task<IActionResult> Create(PhoneNumber number)
         {
-            await _repository.CreateAsync(cont);
-            return Ok(cont);
+            await _repository.CreateAsync(number);
+            //return Ok(feat);
+            return CreatedAtAction(nameof(GetByID), new { id = number.Id }, number);
+
         }
 
         [HttpPut]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> Update(Contact cont)
+        public async Task<IActionResult> Update(PhoneNumber number)
         {
-            await _repository.UpdateAsync(cont);
+            await _repository.UpdateAsync(number);
             return NoContent();
         }
+
 
         [HttpDelete("{id}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
@@ -56,6 +55,7 @@ namespace _00013708_web_application_api.Controllers
             await _repository.DeleteAsync(id);
             return NoContent();
         }
+
 
     }
 }
