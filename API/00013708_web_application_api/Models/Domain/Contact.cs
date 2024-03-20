6918ce9830d685bf6b34a66a72ee2c2cb7a1f9cd
wiut00013708 <wiut00013708@gmail.com>
2024-03-20 16:57:07 +0500

    Add domain classes

---
+++ API/00013708_web_application_api/Models/Domain/Contact.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _00013708_web_application_api.Models.Domain
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ API/00013708_web_application_api/Models/Domain/PhoneNumber.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _00013708_web_application_api.Models.Domain
+{
+    public class PhoneNumber
+    {
+        public int Id { get; set; }
+        
+        [ForeignKey("ContactId")]
+        public int? ContactId { get; set; }
+        public Contact? contact { get; set; }
+        public string Number { get; set; }
+        public string Type { get; set; }
+
+
+    }
+}
