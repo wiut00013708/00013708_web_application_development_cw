4b7b0b1549a17b16cc1970a803b27876356f4527
wiut00013708 <wiut00013708@gmail.com>
2024-03-21 16:20:25 +0500

    set up routing

--- UI/00013708_web_application_ui/src/app/app.component.ts
+++ UI/00013708_web_application_ui/src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
+import { Numbers } from './Numbers';
+import { NumbersComponent } from './components/numbers/numbers.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule],
+  imports: [RouterOutlet, MatButtonModule, NavigationComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '00013708_web_application_ui';
+
 }
--- UI/00013708_web_application_ui/src/app/app.routes.ts
+++ UI/00013708_web_application_ui/src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { NumbersComponent } from './components/numbers/numbers.component';
+import { CreateComponent } from './components/create/create.component';
+import { DetailsComponent } from './components/details/details.component';
+import { EditComponent } from './components/edit/edit.component';
+import { DeleteComponent } from './components/delete/delete.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: NumbersComponent
+    },
+
+    {
+        path: "numbers",
+        component: NumbersComponent
+    },
+
+    {
+        path: "create",
+        component: CreateComponent
+    },
+
+    {
+        path: "details/:id",
+        component: DetailsComponent
+    },
+    {
+        path: "edit/:id",
+        component: EditComponent
+    },
+    {
+        path: "delete/:id",
+        component: DeleteComponent
+    }
+];
